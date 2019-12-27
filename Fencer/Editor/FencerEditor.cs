6725ab916cd2646ee85b1e421a597551999abcb9
dasparadoxon <dasparadoxon@users.noreply.github.com>
2019-12-27 11:12:28 +0100

    initial commit

---
+++ Fencer/Editor/FencerEditor.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FencerUtility
+{
+
+    [CustomEditor(typeof(Fencer))]
+    public class FencerEditor : Editor
+    {
+        public bool debugMode = true;
+
+        public Vector2 currentMousePosition;
+
+        private Fencer fencerInstance;
+
+        public override void OnInspectorGUI()
+        {
+
+            //dbg("Current Event OnInspectorGUI : " + Event.current.type.ToString());
+
+            base.OnInspectorGUI();
+
+            Fencer fencerInstance = (Fencer)target;
+
+            if (!fencerInstance.CreatingMode)
+            {
+                // draw button will only be shown if the fencer is not allready in editing mode
+                if (GUILayout.Button("Draw"))
+                {
+                    fencerInstance.CreateFence();
+                }
+            }
+            else
+            {
+
+                // button for closing the polygon, meaning the last point will be connected to the first polygon point
+                if (GUILayout.Button("Close Polygon"))
+                {
+                    fencerInstance.isPolygon = true;
+                    fencerInstance.CloseFence();
+                    
+                }
+
+                // button for ending the drawing without connecting the last point to the first polygon point
+                if (GUILayout.Button("Finish without Closing"))
+                {
+                    fencerInstance.isPolygon = false;
+                    fencerInstance.CloseFence();
+                    
+                }
+            }
+
+            // if there are more than two points drawn, the button for playing the fence elements on the edges will be shown
+            if (fencerInstance.FencePoints.Count > 2)
+                if (GUILayout.Button("Place Fence"))
+                {
+                    fencerInstance.GenerateFence();
+                }
+
+
+        }
+
+        public void OnSceneGUI()
+        {
+
+            // get the current instance of the gameobject this script is attached to
+            fencerInstance = (Fencer)this.target;
+
+            if(fencerInstance.GetComponent<Collider>()==null)
+            {
+                dbg("The Object to place the fence on MUST have some sort of collider attached to it.",true);
+            }
+
+            // paints the fence polygon if there is one and the repaint event is the current event
+            if (Event.current.type == EventType.Repaint)
+            {
+
+                List<Vector3> fencePoints = new List<Vector3>();
+
+                if(fencerInstance.FencePoints != null)
+                    fencePoints = fencerInstance.FencePoints;
+
+                float fencePointDiscRadius = 1f;
+
+                for (int fencePointIndex = 0; fencePointIndex < fencePoints.Count; fencePointIndex++)
+                {
+
+                    if (fencePointIndex == 0)
+                    {
+                        Handles.color = Color.green;
+                        Handles.DrawSolidDisc(fencePoints[fencePointIndex], Vector3.up, fencePointDiscRadius * 3f);
+
+                        if (!fencerInstance.CreatingMode & fencePoints.Count > 0)
+                        {
+                            Handles.DrawLine(fencePoints[fencePointIndex], fencePoints[fencePoints.Count - 1]);
+                        }
+                    }
+                    else
+                    {
+                        Handles.color = Color.white;
+                        Handles.DrawSolidDisc(fencePoints[fencePointIndex], Vector3.up, fencePointDiscRadius);
+                        //if(fencePoints.Count > 2)
+                        //    Handles.DrawLine(fencePoints[fencePointIndex],fencePoints[fencePointIndex+1]);
+                        if (fencerInstance.CreatingMode & fencePointIndex > 0)
+                        {
+                            Handles.DrawLine(fencePoints[fencePointIndex], fencePoints[fencePointIndex - 1]);
+                        }
+
+                        if (!fencerInstance.CreatingMode & fencePointIndex > 0)
+                        {
+                            Handles.DrawLine(fencePoints[fencePointIndex], fencePoints[fencePointIndex - 1]);
+                        }
+                    }
+
+
+
+                }
+
+
+
+
+            }
+
+            // if Fencer is in Creating mode, then check the apropriate Events and draw mouse mausposition and partial line
+            if (fencerInstance.CreatingMode)
+            {
+
+                this.DrawCurrentMousePositionAndCurrentFencePartLine();
+
+                if (Event.current.type == EventType.Layout)
+                {
+
+                    HandleUtility.AddDefaultControl(GUIUtility.GetControlID(GetHashCode(), FocusType.Passive));
+                }
+
+                // update mouse position for the drawing of the current next fence point
+                // needed for the case the mouse doesnt move at all
+                if (Event.current.type == EventType.MouseMove)
+                {
+
+                    this.currentMousePosition = Event.current.mousePosition;
+                    Event.current.Use();
+                }
+
+
+                if (Event.current.type == EventType.MouseDown)
+                {
+
+                    Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit))
+                    {
+
+                        if (hit.collider.gameObject == fencerInstance.gameObject)
+                        {
+                            fencerInstance.AddFencePoint(hit.point);
+                        }
+
+                        //Debug.DrawLine(ray.origin, hit.point, Color.red);
+                        //Debug.Log(hit.point);
+                        //dbg("Mouse Hit clicked on Scene View at : " + hit.point.ToString());
+                        //dbg("Hit Object with Name : "+hit.collider.gameObject.name);
+
+                    }
+
+                    Event.current.Use();
+
+                }
+
+
+            }
+
+        }
+
+        private void DrawCurrentMousePositionAndCurrentFencePartLine()
+        {
+            // draw a sold disc at the current mouse position
+            Ray rayMousePointer = HandleUtility.GUIPointToWorldRay(this.currentMousePosition);
+
+            RaycastHit hitMousePointer;
+
+            if (Physics.Raycast(rayMousePointer, out hitMousePointer))
+            {
+                if (hitMousePointer.collider.gameObject == fencerInstance.gameObject)
+                {
+
+                    Handles.DrawSolidDisc(hitMousePointer.point, Vector3.up, 2f);
+
+                    // Draw a line from the last fence point to the current mouse position
+                    if (fencerInstance.FencePoints.Count > 0)
+                    {
+                        Vector3 previousSetFencerPoint = fencerInstance.FencePoints[fencerInstance.FencePoints.Count - 1];
+
+                        // get the direction normal and the length of the current potential fence point relativ to the last set
+                        Vector3 direction = (hitMousePointer.point - previousSetFencerPoint).normalized;
+
+                        float length = (hitMousePointer.point - previousSetFencerPoint).magnitude;
+
+                        // get the size of the fence prefab element
+                        Bounds fencePrefabBounds = fencerInstance.fencePrefab.GetComponent<Renderer>().bounds;
+
+                        float lengthOfFencePrefab = fencePrefabBounds.size.x;
+
+                        // calculate how many elements of the prefab would fit
+                        int numberOfFencesThatWouldFitOnTheNewLineSegment = (int)(length / lengthOfFencePrefab);
+
+                        for (int i = 0; i < numberOfFencesThatWouldFitOnTheNewLineSegment; i++)
+                        {
+                            Handles.color = Color.red;
+                            Handles.DrawSolidDisc(previousSetFencerPoint + ((direction * lengthOfFencePrefab) * (i + 1)), Vector3.up, 0.5f);
+                        }
+
+                        Handles.color = Color.white;
+                        Handles.DrawLine(previousSetFencerPoint, hitMousePointer.point);
+                    }
+                }
+            }
+        }
+
+        protected void dbg(string message, bool error = false)
+        {
+            if (debugMode & !error)
+                Debug.Log("[ " + this.GetType().Name + " (" + Time.time + ")] " + message);
+
+            if (error)
+                Debug.LogError("[" + this.GetType().Name + " (" + Time.time + ")] " + message);
+        }
+    }
+
+}
---
+++ Fencer/Scripts/Fencer.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FencerUtility
+{
+
+    public class Fencer : MonoBehaviour
+    {
+        #region Public Fields
+
+        public bool debugMode = false;
+
+        public bool creatingMode = false;
+
+        public GameObject fencePrefab = null;
+
+        public GameObject fenceContainer = null;
+
+        public bool isPolygon = true;
+
+        #endregion
+
+        #region Private Fields
+
+        public List<Vector3> fencePoints;
+
+        public List<Vector3> FencePoints { get => fencePoints; set => fencePoints = value; }
+        public bool CreatingMode { get => creatingMode; set => creatingMode = value; }
+        #endregion
+
+
+        #region Unity Methods
+
+        public void OnValidate()
+        {
+        }
+
+        #endregion
+
+        #region Class Methods
+
+        /// Is called by the Editor Class when the fence should be drawn and the button is clicked in the editor
+        public void CreateFence()
+        {
+
+            // only start fence drawing if there is an actual fence prefab set
+            if (this.fencePrefab != null)
+            {
+                this.CreatingMode = true;
+                this.FencePoints.Clear();
+            }
+            else
+                dbg("You need to set a prefab fence element !", true);
+
+        }
+
+        /// Is called by the Editor class when the user wants to close the fence polygon
+        public void CloseFence()
+        {
+
+            this.CreatingMode = false;
+        }
+
+        public void GenerateFence()
+        {
+
+            // if there is no fenceContainer, create a new one
+            if (fenceContainer == null)
+            {
+                GameObject newFenceContainer = new GameObject();
+
+                newFenceContainer.name = "Unnamend Fence Container";
+
+                this.fenceContainer = newFenceContainer;
+
+            }
+
+            for (var currentFencePointIndex = 0; currentFencePointIndex < this.fencePoints.Count; currentFencePointIndex++)
+            {
+
+                Vector3 firstPoint = this.fencePoints[currentFencePointIndex];
+
+                Vector3 secondPoint = Vector3.zero;
+
+                // if it is the last point, then the second point has to be the start point of the fencepoints
+                if (currentFencePointIndex == (this.fencePoints.Count - 1))
+                    secondPoint = this.fencePoints[0];
+                else
+                    secondPoint = this.fencePoints[currentFencePointIndex + 1];
+
+                // now I need the angle of the edge consisting of both points relative to the X axsis.
+
+                // make it relative to the origin (0,0,0)
+                Vector3 edgeVector = secondPoint - firstPoint;
+
+                // calculate the signed angle between the positive x-axis and the edge
+                float signedAngleBetweenXAxisAndCurrentEdge = Vector3.SignedAngle(Vector3.right, edgeVector, Vector3.up);
+
+                // with this angle we need to fill the whole edge with the according number of fence elements
+
+                // get the direction normal and the length of the current potential fence point relativ to the last set
+                Vector3 direction = (secondPoint - firstPoint).normalized;
+
+                float length = (secondPoint - firstPoint).magnitude;
+
+                // get the size of the fence prefab element
+                Bounds fencePrefabBounds = this.fencePrefab.GetComponent<Renderer>().bounds;
+
+                float lengthOfFencePrefab = fencePrefabBounds.size.x;
+
+                // calculate how many elements of the prefab would fit
+                int numberOfFencesThatWouldFitOnTheNewLineSegment = (int)(length / lengthOfFencePrefab);
+
+                for (int i = 0; i < numberOfFencesThatWouldFitOnTheNewLineSegment; i++)
+                {
+                    Vector3 positionOfFenceElement = firstPoint + ((direction * lengthOfFencePrefab) * (i));
+
+                    Vector3 firstPointPartialEdgeForFenceElement = positionOfFenceElement;
+
+                    Vector3 secondPointPartialEdgeForFenceElement = firstPoint + ((direction * lengthOfFencePrefab) * (i + 1));
+
+                    // now to get the correct y-axis (height) for each of the two fence element corner points
+
+                    RaycastHit hit;
+                    // Does the ray intersect any objects excluding the player layer
+                    if (Physics.Raycast(firstPointPartialEdgeForFenceElement + new Vector3(0,5f,0), transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+                    {
+
+                        if (hit.collider.gameObject == this.gameObject)
+                        {
+                            firstPointPartialEdgeForFenceElement.y = hit.point.y;
+                            positionOfFenceElement.y = hit.point.y;
+                        }
+                    }
+
+                    if (Physics.Raycast(secondPointPartialEdgeForFenceElement + new Vector3(0,5f,0) , transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+                    {
+                        if (hit.collider.gameObject == this.gameObject)
+                        {
+                            secondPointPartialEdgeForFenceElement.y = hit.point.y;
+                        }
+                    }
+
+                    // transform it to origin zero
+                    Vector3 partialEdgeVector = secondPointPartialEdgeForFenceElement - firstPointPartialEdgeForFenceElement;
+
+                    // calculate the signed angle between the positive x-axis and the edge, in other words the stepness of the current edge part
+
+                    Vector3 point1;
+                    Vector3 point2;
+
+                    point1 = partialEdgeVector;
+
+                    point2 = point1.normalized * point1.magnitude;
+                    point2.y = 0;
+
+                    Vector3 normalVectorOfBothEdges = Vector3.Cross(point1, point2);
+
+                    if (point1.z >= 0 && point1.x >= 0)
+                        normalVectorOfBothEdges = Vector3.Cross(point1, point2);
+
+                    if (point1.z < 0 && point1.x >= 0)
+                        normalVectorOfBothEdges = Vector3.Cross(point2, point1);
+
+                    if (point1.z < 0 && point1.x < 0)
+                        normalVectorOfBothEdges = Vector3.Cross(point2, point1);
+
+                    if (point1.z >= 0 && point1.x < 0)
+                        normalVectorOfBothEdges = Vector3.Cross(point1, point2);
+
+                    if (point1.z < 0 && point1.x >= 0 && point1.y < 0)
+                        normalVectorOfBothEdges = Vector3.Cross(point1, point2);
+
+                    if (point1.z < 0 && point1.x < 0 && point1.y < 0)
+                        normalVectorOfBothEdges = Vector3.Cross(point1, point2);
+
+                    if (point1.z >= 0 && point1.x < 0 && point1.y >= 0)
+                        normalVectorOfBothEdges = Vector3.Cross(point2, point1);
+
+                    Vector3 positionCrossDot = normalVectorOfBothEdges.normalized * 0.5f;
+
+                    float signedAngle = Vector3.SignedAngle(point2, point1, positionCrossDot);
+
+                    GameObject newFenceElement = Instantiate
+                    (
+                        this.fencePrefab,
+                        positionOfFenceElement,
+                        Quaternion.Euler(0, 0, 0)
+                    );
+
+                    newFenceElement.transform.Rotate(new Vector3(0, signedAngleBetweenXAxisAndCurrentEdge, 0), Space.Self);
+
+                    newFenceElement.transform.Rotate(new Vector3(0, 0, signedAngle), Space.Self);
+
+                    /*newFenceElement.name = "[" + i + "] | P1 : " + point1.ToString() + " | P2 : " + point2.ToString();*/
+
+                    newFenceElement.name += " (" + i + ")";
+
+                    newFenceElement.transform.parent = this.fenceContainer.transform;
+
+
+
+
+
+                }
+
+
+
+
+            }
+
+
+
+        }
+
+        public void AddFencePoint(Vector3 newFencePoint)
+        {
+
+            if (this.fencePoints == null)
+                this.fencePoints = new List<Vector3>();
+
+            this.fencePoints.Add(newFencePoint);
+        }
+
+        protected void dbg(string message, bool error = false)
+        {
+            if (debugMode & !error)
+                Debug.Log("[ " + this.GetType().Name + " (" + Time.time + ")] " + message);
+
+            if (error)
+                Debug.LogError("[" + this.GetType().Name + " (" + Time.time + ")] " + message);
+        }
+
+        #endregion
+    }
+
+}
